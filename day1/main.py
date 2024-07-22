f1f79d7161087197f164daf1c276f8aacbe3e254
Alexander Zayats <iamnotzayats@yandex.ru>
2024-07-22 20:53:10 +0300

    Complete day 1: How to use input and output

---
+++ day1/main.py
@@ -0,0 +1,40 @@
+# print('Hello World')
+# # Hello World comment 
+
+# print("Hello 'World'")
+# print('Hello \"World\"')
+
+# print("Hello\nWorld")
+
+# print("Hello " + "," + " World")
+
+
+# how to create comment: ctrl + /
+# print("Hello, " + input("What is your name?"))
+
+# name = input("What is your name?")
+# print(name)
+
+
+# name_of_jack = "Jackl"
+# print(name_of_jack)
+
+# random_name = input("Enter a random name: ")
+# lenght_random_name = len(random_name)
+# print(random_name, lenght_random_name)
+
+
+print("Welcome to my first program in Python")
+person_name = input("Tell me please, what is your name?\n")
+print("Nice to meet u, "+ person_name)
+person_city = input("Now I want to know what city did you grow up in?\n")
+print(person_city + " is a good city!")
+person_pet_name = input("And the last question, tell me what is the name of your pet?\n")
+print("Ha-ha, very nice. I didn't even know what his or her name was!")
+print("Now I know your name, where you grew up and the name of your pet!\n")
+print("Your name is: " + person_name + "\nCity where you grew up: " + person_city + "\nAnd your pet's name is: " + person_pet_name)
+
+
+
+
+
